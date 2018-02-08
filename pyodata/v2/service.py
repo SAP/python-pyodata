07d8a07e009ef8f91e83120a9b0007131bc6c824
Patrik Petrik <patrik.petrik@sap.com>
2018-02-08 16:50:31 +0100

    Navigate also from EntityProxy object

--- pyodata/v2/service.py
+++ pyodata/v2/service.py
@@ -692,6 +692,32 @@ class EntityProxy(object):
                 raise AttributeError('EntityType {0} does not have Property {1}: {2}'
                                      .format(self._entity_type.name, attr, ex.message))
 
+    def nav(self, nav_property):
+        """Navigates to given navigation property and returns the EntritySetProxy"""
+
+        # for now duplicated with simillar method in entity set proxy class
+        try:
+            navigation_property = self._entity_set.entity_type.nav_proprty(nav_property)
+        except KeyError:
+            raise PyODataException('Navigation property {} is not declared in {} entity type'.format(
+                    nav_property, self._entity_set.entity_type))
+
+        # Get entity set of navigation property
+        association_set = self._service.schema.association_set_by_association(navigation_property.association, navigation_property.association_info.namespace)
+        navigation_entity_set = None
+        for entity_set in association_set.end_roles:
+            if association_set.end_roles[entity_set] == navigation_property.to_role.role:
+                navigation_entity_set = entity_set
+        if not navigation_entity_set:
+            raise PyODataException('No association set for role {}'.format(navigation_property.to_role))
+
+        return EntitySetProxy(
+            self._service, 
+            self._service.schema.entity_set(navigation_entity_set), 
+            nav_property,
+            self._entity_set.name + self._entity_key.to_key_string()
+        )
+
     def get_proprty(self, name, connection=None):
         """Returns value of the property"""
 
--- tests/test_service_v2.py
+++ tests/test_service_v2.py
@@ -379,6 +379,106 @@ def test_navigation(service):
 
 
 @responses.activate
+def test_navigation_from_entity_multi(service):
+    """Get entities via navigation property from entity proxy"""
+
+    # pylint: disable=redefined-outer-name
+
+    responses.add(
+        responses.GET,
+        "{0}/Employees(23)".format(service.url),
+        json={'d': {
+            'ID': 23,
+            'NameFirst': 'Rob',
+            'NameLast': 'Ickes'
+        }},
+        status=200)
+
+    responses.add(
+        responses.GET,
+        "{0}/Employees(23)/Addresses".format(service.url),
+        json={'d': {
+            'results': [
+                {
+                    'ID': 456,
+                    'Street': 'Baker Street',
+                    'City': 'London'
+                },{
+                    'ID': 457,
+                    'Street': 'Lowth Road',
+                    'City': 'London'
+                },{
+                    'ID': 458,
+                    'Street': 'Warner Road',
+                    'City': 'London'
+                }
+            ]
+        }},
+        status=200)
+
+    request = service.entity_sets.Employees.get_entity(23)
+
+    assert isinstance(request, pyodata.v2.service.EntityGetRequest)
+
+    emp = request.execute()
+
+    request = emp.nav('Addresses').get_entities()
+
+    assert isinstance(request, pyodata.v2.service.QueryRequest)
+
+    addrs = request.execute()
+    assert addrs[0].ID == 456
+    assert addrs[0].Street == 'Baker Street'
+    assert addrs[0].City == 'London'
+    assert addrs[1].ID == 457
+    assert addrs[1].Street == 'Lowth Road'
+    assert addrs[1].City == 'London'
+    assert addrs[2].ID == 458
+    assert addrs[2].Street == 'Warner Road'
+    assert addrs[2].City == 'London'
+
+
+@responses.activate
+def test_navigation_from_entity(service):
+    """Check getting entity via navigation property from entity proxy"""
+
+    # pylint: disable=redefined-outer-name
+
+    responses.add(
+        responses.GET,
+        "{0}/Employees(23)".format(service.url),
+        json={'d': {
+            'ID': 23,
+            'NameFirst': 'Rob',
+            'NameLast': 'Ickes'
+        }},
+        status=200)
+
+    responses.add(
+        responses.GET,
+        "{0}/Employees(23)/Addresses(456)".format(service.url),
+        json={'d': {
+            'ID': 456,
+            'Street': 'Baker Street',
+            'City': 'London'
+        }},
+        status=200)
+
+    request = service.entity_sets.Employees.get_entity(23)
+
+    assert isinstance(request, pyodata.v2.service.EntityGetRequest)
+
+    emp = request.execute()
+
+    request = emp.nav('Addresses').get_entity(456)
+
+    addr = request.execute()
+    assert addr.ID == 456
+    assert addr.Street == 'Baker Street'
+    assert addr.City == 'London'
+
+
+@responses.activate
 def test_get_entity(service):
     """Check getting entities"""
 
