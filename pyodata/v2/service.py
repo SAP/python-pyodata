a545948b11422e1bdae07d46dee733e6c6b3cd41
Jakub Filak <jakub.filak@sap.com>
2020-03-05 11:50:33 +0100

    service: do not read response.d if not exists
    Eh, that's a stupid approach but I have no time to think.

--- pyodata/v2/service.py
+++ pyodata/v2/service.py
@@ -841,7 +841,11 @@ class EntityProxy:
                 raise HttpError('HTTP GET for Attribute {0} of Entity {1} failed with status code {2}'
                                 .format(proprty.name, key, response.status_code), response)
 
-            data = response.json()['d']
+            try:
+                data = response.json()['d']
+            except KeyError:
+                data = response.json()
+
             return proprty.typ.traits.from_json(data[proprty.name])
 
         path = urljoin(self.get_path(), name)
@@ -1043,7 +1047,10 @@ class EntitySetProxy:
                 raise HttpError('HTTP GET for Entity {0} failed with status code {1}'
                                 .format(self._name, response.status_code), response)
 
-            entity = response.json()['d']
+            try:
+                entity = response.json()['d']
+            except KeyError:
+                entity = response.json()
 
             return NavEntityProxy(parent, nav_property, navigation_entity_set.entity_type, entity)
 
@@ -1071,7 +1078,10 @@ class EntitySetProxy:
                 raise HttpError('HTTP GET for Entity {0} failed with status code {1}'
                                 .format(self._name, response.status_code), response)
 
-            entity = response.json()['d']
+            try:
+                entity = response.json()['d']
+            except KeyError:
+                entity = response.json()
 
             return EntityProxy(self._service, self._entity_set, self._entity_set.entity_type, entity)
 
@@ -1125,7 +1135,11 @@ class EntitySetProxy:
                 raise HttpError('HTTP POST for Entity Set {0} failed with status code {1}'
                                 .format(self._name, response.status_code), response)
 
-            entity_props = response.json()['d']
+
+            try:
+                entity_props = response.json()['d']
+            except KeyError:
+                entity_props = response.json()
 
             return EntityProxy(self._service, self._entity_set, self._entity_set.entity_type, entity_props)
 
@@ -1260,7 +1274,10 @@ class FunctionContainer:
                     'The Function Import %s has replied with HTTP Status Code %d instead of 200',
                     fimport.name, response.status_code)
 
-            response_data = response.json()['d']
+            try:
+                response_data = response.json()['d']
+            except KeyError:
+                response_data = response.json()
 
             # 1. if return types is "entity type", return instance of appropriate entity proxy
             if isinstance(fimport.return_type, model.EntityType):
